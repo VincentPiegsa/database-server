d9adf240a65bc8d285d7382a4a9e4931ac9f225b
VincentPiegsa <33578820+VincentPiegsa@users.noreply.github.com>
2018-10-22 17:08:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 80;
+const databaseFile = './db/data.db';
+const usersDatabase = './db/users.db';
+
+app.use(express.static(path.join(__dirname, '/static')));
+app.use(express.static(path.join(__dirname, '/db')));
+app.use(express.static(path.join(__dirname, '/images')));
+app.use(bodyParser());
+
+let db = new sqlite3.Database(databaseFile, (error) => {
+    if (error) throw error;
+    console.log('Connected to SQLite3 database.');
+});
+
+let users = new sqlite3.Database(usersDatabase, (error) => {
+    if (error) throw error;
+    console.log("Connected to Authentication Database");
+});
+
+const fetchDatabase = function(sql, res) {
+    db.all(sql, (error, rows) => {
+        if (error) {
+            res.status(404);
+            res.send(`${error}`);
+        } else {
+            res.status(200);
+            res.send(rows);
+        }
+    });
+};
+
+app.get(['/', '/home'], (req, res) => {
+    res.status(200);
+    res.sendFile(__dirname + '/static/home.html');
+});
+
+app.get('/project/sensor', (req, res) => {
+    res.status(200);
+    res.sendFile(__dirname + '/static/project_sensor.html');
+});
+
+app.get('/project/server', (req, res) => {
+    res.status(200);
+    res.sendFile(__dirname + '/static/project_server.html');
+});
+
+app.get('/project/analysis', (req, res) => {
+    res.status(200);
+    res.sendFile(__dirname + '/static/project_analysis.html');
+});
+
+app.get('/database', (req, res) => {
+    res.status(200);
+    res.sendFile(__dirname + '/static/database.html');
+});
+
+app.post('/database/query', (req, res) => {
+    fetchDatabase(req.body.sql, res);
+});
+
+app.get('/contact', (req, res) => {
+    res.status(200);
+    res.sendFile(__dirname + '/static/contact.html');
+});
+
+app.get('/images/wetterstation1.jpg', (req, res) => {
+    res.sendFile(__dirname + '/images/wetterstation1.jpg');
+});
+
+app.get('/images/wetterstation2.jpg', (req, res) => {
+    res.sendFile(__dirname + '/images/wetterstation2.jpg');
+});
+
+app.get('/images/arduinoMega.jpg', (req, res) => {
+    res.sendFile(__dirname + '/images/arduino_mega.jpg');
+});
+
+app.get('/images/dataAVG.jpg', (req, res) => {
+    res.sendFile(__dirname + '/images/combinedAVG.jpg');
+});
+
+app.get('/images/tmpAVG.jpg', (req, res) => {
+    res.sendFile(__dirname + '/images/temperatureAVG.jpg');
+});
+
+const server = app.listen(port, () => {console.log(`Listening on Port ${port}...`)});
